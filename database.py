3696a23e6156374d128b2eade3427c61a1a515fe
hayz0101 <joferrer@iskolarngbayan.pup.edu>
2022-07-31 14:28:40 +0800

    database name change to 'homies_logistics'

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost/warehousing_system"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost/homies_logistics"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=100, pool_recycle=280, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
