2d72224e52b05b387383fbe66633dfbbad442381
Jeffx021 <jeffersonborjal019@gmail.com>
2022-09-10 15:57:42 +0800

    fixed UI bugs, login bugs projms

--- static/project_management/department_head/department_head_footer_scripts.js
+++ static/project_management/department_head/department_head_footer_scripts.js
@@ -2,26 +2,12 @@
 
 $('#logout_form').on('submit', function (e) {
     e.preventDefault();
-    sessionStorage.clear();
     window.location.replace('/')
+    sessionStorage.clear();
 });
 
-// $(document).ready(function () {
-//     $.ajax({
-//         url: '/employee/user/' + user_id,
-//         type: "GET",
-//         dataType: "JSON",
-
-//         success: function (data) {
-//             var first_name = data.first_name.charAt(0).toUpperCase() + data.first_name.slice(1);
-//             var last_name = data.last_name.charAt(0).toUpperCase() + data.last_name.slice(1);
-//             var suffix_name = data.suffix_name.charAt(0).toUpperCase() + data.suffix_name.slice(1);
-//             $('#full_name').html(first_name + ' ' + last_name + ' ' + suffix_name);
-
-//             department = data.departments.name
-//             department_id = data.department_id
-
-//             $('#department').html(department);
-//         }
-//     })
-// });
+$(document).ready(function () {
+    $('#full_name').html(sessionStorage.getItem("name"));
+    $('#user_type').html(sessionStorage.getItem("type"));
+    $('#department').html(sessionStorage.getItem("DEPARTMENTNAME"));
+});
